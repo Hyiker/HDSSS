92325f9aa1ed1f1f81c40622414396b8cf3177c0
Carbene <hyikerhu0212@gmail.com>
2023-05-06 14:45:18 +0800

    move glad dependency

--- loo/xmake.lua
+++ loo/xmake.lua
@@ -3,6 +3,7 @@ add_rules("mode.debug", "mode.release")
 add_requires("glfw 3.3.8", "glm 0.9.9+8", "glog v0.6.0", "assimp v5.2.5", "meshoptimizer v0.18", "stb 2023.01.30")
 add_requires("imgui v1.89", {configs = {glfw_opengl3 = true, use_glad = true}})
 add_requires("glslang", {configs = {binaryonly = true}})
+add_requires("glad")
 
 package("meshoptimizer")
     set_homepage("https://github.com/zeux/meshoptimizer")
@@ -42,23 +43,21 @@ target("loo")
     add_includedirs(".", {public = true})
 
     add_files("src/*.cpp")
-    add_packages("glfw", "glm", "glog", "imgui", "assimp", "meshoptimizer", "stb", {public = true})
+    add_packages("glfw", "glm", "glog", "imgui", "assimp", "meshoptimizer", "stb", "glad", {public = true})
 
     -- glad
     if is_plat("macosx") then
         -- macosx offers the latest support until 4.3
-        ogl_ver = "43"
+        ogl_ver = "41"
     else
         ogl_ver = "46"
     end
     add_defines("OGL_" .. ogl_ver, "_USE_MATH_DEFINES", "NOMINMAX", {public = true})
-    add_includedirs(string.format("ext/glad%s/include", ogl_ver), {public = true})
-    add_files(string.format("ext/glad%s/src/glad.c", ogl_ver), {public = true})
 
     on_config(function (target)
         local ogl_ver = "4.6"
         if is_plat("macosx") then
-            ogl_ver = "4.6"
+            ogl_ver = "4.1"
         end
         cprintf("${bright green}[INFO] ${clear}glad configure using opengl %s on %s\n", ogl_ver, os.host())
     end)
